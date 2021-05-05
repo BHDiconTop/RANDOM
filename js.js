3c2d8f9aaccc93232424fda9c063e64a892e962e
BHDiconTop <83541031+BHDiconTop@users.noreply.github.com>
2021-05-05 21:15:17 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+var cards_block = document.querySelector('.cards');
+var cards = document.querySelectorAll('.cards > div');
+
+function start() {
+  var random = Math.floor(Math.random() * 32); // От 0 до 8
+  cards_block.style.left = -random * 50 + 'px';
+};
+var win = document.querySelector('.win').onclick = function() {
+	alert('good');
+}
